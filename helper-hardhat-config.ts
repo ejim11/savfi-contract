d82aa28e6475bb077afd04785c3c87450f8bc738
ejim favour <favourejim56@gmail.com>
2023-08-01 16:11:04 +0100

    added helper hardhat config file

---
+++ helper-hardhat-config.ts
@@ -0,0 +1,11 @@
+export const networkConfig = {
+    11155111: {
+        name: "sepolia",
+    },
+    31337: {
+        name: "hardhat",
+    },
+}
+
+export const developmentChains: string[] = ["hardhat", "localhost"]
+
